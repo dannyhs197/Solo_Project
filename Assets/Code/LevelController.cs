3c8834896be1496ae49fa3ffb9214565f1509724
dannyhs197 <A00281504@student.ait.ie>
2021-11-17 15:44:38 +0000

    Basic Level Transitions

---
+++ Assets/Code/LevelController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); ;
+
+
+            //Restart
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex;
+        }
+    }
+}
